b9b23c1dd52eeeba0fb0a08a8b96de9641592b1c
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-08-15 13:23:22 +0300

    Delete user is working

--- Controllers/user-controllers.js
+++ Controllers/user-controllers.js
@@ -59,6 +59,19 @@ const userController = {
         })
         .catch((err) => res.status(400).json(err));
     },
+
+    //Delete a user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({_id: params.id })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No User found with this ID"});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => res.status(400).json(err));
+    },
 };
 
 module.exports = userController;
--- Routes/api/user-routes.js
+++ Routes/api/user-routes.js
@@ -5,12 +5,13 @@ const {
     createUser,
     getUserById,
     updateUser,
+    deleteUser,
     
 } = require('../../Controllers/user-controllers');
 //Get all users and Post users
 router.route('/').get(getAllUsers).post(createUser);
 
-//Get one user by ID
-router.route('/:id').get(getUserById).put(updateUser);
+//Get one user by ID, PUT,and DELTE Aa user
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
